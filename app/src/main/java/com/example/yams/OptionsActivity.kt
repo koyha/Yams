27647ad9c944c8aaf1bf52b2ae37072581ee4ad7
Galaad Moll <galaad@d-centralize.nl>
2021-09-01 12:24:49 +0200

    add header with back button

--- app/src/main/java/com/example/yams/OptionsActivity.kt
+++ app/src/main/java/com/example/yams/OptionsActivity.kt
@@ -4,17 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class OptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
 
-        val buttonHomepage: Button = findViewById (R.id.button_options_to_homepage)
-
-        buttonHomepage.setOnClickListener() {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        val backButton: ImageButton = findViewById (R.id.back_button)
+        backButton.setOnClickListener() {
+            finish()
         }
     }
 }
