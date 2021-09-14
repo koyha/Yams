a97300e9549289c0f8ce40c434522111dbe79c62
koyha <louislaniece@gmail.com>
2021-09-14 10:27:58 +0200

    + tabs des Fragments(1 par joueur)
    - link de l'update des scores

--- app/src/main/java/com/example/yams/GameActivity.kt
+++ app/src/main/java/com/example/yams/GameActivity.kt
@@ -2,9 +2,15 @@ package com.example.yams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 
 class GameActivity : AppCompatActivity() {
+
+    lateinit var  option: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
@@ -12,27 +18,48 @@ class GameActivity : AppCompatActivity() {
         val bund: Bundle? = this.intent.extras
         val playersName = bund?.getStringArrayList("playersName")
 
+        val fragments : ArrayList<FragmentGrille> = ArrayList()
+        val fragmentsHashMap : HashMap<String, FragmentGrille> = HashMap()
+
         if (playersName != null) {
             for (item in playersName)
                 if (item != null) {
-                    val playerName = TextView(this)
-                    playerName.text = item
+                    val fragment: FragmentGrille = FragmentGrille.newInstance(item,item)
+                    fragments.add(fragment)
+                    fragmentsHashMap.put(item, fragment)
                 }
+        }
+
+        val score = InputScore()
+        val dice = supportFragmentManager.findFragmentById(R.id.input_dice) as InputDice
+        dice.score = score
+
+        option = findViewById<Spinner>(R.id.spinner)
+        option.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playersName as ArrayList<String>)
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener,
+            AdapterView.OnItemClickListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                supportFragmentManager.beginTransaction().replace(R.id.score_table, fragments[position]).commit()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                //fragments[position].onInputChange()
+            }
         }
     }
 
     override fun onStart() {
         super.onStart()
-
-        val score = InputScore()
-        val fragment = supportFragmentManager.findFragmentById(R.id.score_table) as FragmentGrille
-        fragment.inputInputScore = score
-        val dice = supportFragmentManager.findFragmentById(R.id.input_dice) as InputDice
-        dice.score = score
     }
 
     fun onUpdateListener() {
-        val fragment = supportFragmentManager.findFragmentById(R.id.score_table) as FragmentGrille
-        fragment.onInputChange()
+        //val fragment = supportFragmentManager.findFragmentById(R.id.score_table) as FragmentGrille
+        //fragment.onInputChange()
     }
 }
