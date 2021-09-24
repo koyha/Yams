0d8d28be3957fa4677c15b169e40a37385293486
koyha <louislaniece@gmail.com>
2021-09-24 19:33:08 +0200

    +add method pour voir tout les files present

--- app/src/main/java/com/example/yams/Storage.kt
+++ app/src/main/java/com/example/yams/Storage.kt
@@ -2,12 +2,45 @@ package com.example.yams
 
 import android.content.Context
 import androidx.fragment.app.Fragment
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
+import java.io.*
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.util.stream.Collectors
 
 class Storage {
+
+    //TODO: Changer le nom du file que l'on save
+    public fun saveFragmentObject(context: Context, myFrag : GlobalScoresheetFragment) {
+        try
+        {
+            val currentTime = LocalDateTime.now().toString()
+            val fos = context.openFileOutput(currentTime, Context.MODE_PRIVATE);
+            val oos = ObjectOutputStream(fos);
+            oos.writeObject(myFrag);
+            oos.close();
+        } catch (e : IOException) {
+            e.printStackTrace();
+        }
+    }
+
+    public fun loadFragmentObject(context: Context, file: String): GlobalScoresheetFragment {
+        //TODO: ajouter un try catch ?
+        var myFrag: GlobalScoresheetFragment
+        val fileInputStream = FileInputStream((context.getFilesDir()).toString().plus("/filename"));
+        val objectInputStream = ObjectInputStream(fileInputStream);
+        myFrag = objectInputStream.readObject() as GlobalScoresheetFragment
+        return myFrag
+    }
+
+    public  fun getStoredFragmentsList(context: Context): ArrayList<String> {
+        val list = ArrayList<String>()
+        val path = context.fileList()
+        for (file in path){
+            list.add(file)
+        }
+        return list
+    }
 
     public fun saveObject(context: Context, myHashMap : HashMap<Int, String>) {
         try
@@ -34,26 +67,4 @@ class Storage {
         }
     }
 
-
-    //TODO: Changer le nom du file que l'on save
-    public fun saveFragmentObject(context: Context, myFrag : GlobalScoresheetFragment) {
-        try
-        {
-            val fos = context.openFileOutput("filename.ser", Context.MODE_PRIVATE);
-            val oos = ObjectOutputStream(fos);
-            oos.writeObject(myFrag);
-            oos.close();
-        } catch (e : IOException) {
-            e.printStackTrace();
-        }
-    }
-
-    public fun loadFragmentObject(context: Context): GlobalScoresheetFragment {
-        //TODO: ajouter un try catch ?
-        var myFrag: GlobalScoresheetFragment
-        val fileInputStream = FileInputStream((context.getFilesDir()).toString().plus("/filename.ser"));
-        val objectInputStream = ObjectInputStream(fileInputStream);
-        myFrag = objectInputStream.readObject() as GlobalScoresheetFragment
-        return myFrag
-    }
 }
