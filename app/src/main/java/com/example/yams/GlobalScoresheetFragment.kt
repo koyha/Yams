6a7b3c004130c89486817a00c7ab0b190b26326c
Galaad Moll <galaad@d-centralize.nl>
2021-09-26 09:24:34 +0200

    set width in sheets and add basic design for global

--- app/src/main/java/com/example/yams/GlobalScoresheetFragment.kt
+++ app/src/main/java/com/example/yams/GlobalScoresheetFragment.kt
@@ -3,13 +3,13 @@ package com.example.yams
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import android.util.DisplayMetrics
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TableLayout
-import android.widget.TableRow
-import android.widget.TextView
+import android.widget.*
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.setMargins
 import java.io.Serializable
 import java.util.ArrayList
 import java.util.*
@@ -31,15 +31,35 @@ class GlobalScoresheetFragment() : Fragment(), Serializable,  Parcelable {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_global_scoresheet, container, false)
 
+        val displayMetrics = DisplayMetrics()
+        this.activity?.windowManager?.defaultDisplay?.getMetrics(displayMetrics)
+        val width = displayMetrics.widthPixels
+        val cellWidth = if (playersName.size > 4) width/6 else 0
+
         for (player in playersName) {
             val table = v.findViewById<TableLayout>(R.id.scoresheet)
             var row: TableRow = table.getChildAt(0) as TableRow
             var text = TextView(context)
+
+            text.background = ContextCompat.getColor(requireContext(), R.color.background_box).toDrawable()
+            text.layoutParams = TableRow.LayoutParams(cellWidth, TableRow.LayoutParams.MATCH_PARENT)
+            (text.layoutParams as ViewGroup.MarginLayoutParams).setMargins(resources.getDimensionPixelSize(R.dimen.margin_box))
+            text.gravity = Gravity.CENTER_VERTICAL
+            text.setPadding(resources.getDimensionPixelSize(R.dimen.text_padding), 0, resources.getDimensionPixelSize(R.dimen.text_padding), 0)
+            text.maxLines = 1
+
             text.text = player
             row.addView(text)
             for (i: Int in 1 until table.childCount) {
                 row = table.getChildAt(i) as TableRow
                 text = TextView(context)
+
+                text.background = ContextCompat.getColor(requireContext(), R.color.background_box).toDrawable()
+                text.layoutParams = TableRow.LayoutParams(cellWidth, TableRow.LayoutParams.MATCH_PARENT)
+                (text.layoutParams as ViewGroup.MarginLayoutParams).setMargins(resources.getDimensionPixelSize(R.dimen.margin_box))
+                text.gravity = Gravity.CENTER_VERTICAL
+                text.setPadding(resources.getDimensionPixelSize(R.dimen.text_padding), 0, 0, 0)
+
                 row.addView(text)
             }
         }
